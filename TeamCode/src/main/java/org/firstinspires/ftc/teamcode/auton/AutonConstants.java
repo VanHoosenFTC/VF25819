253149e4000dc1fe19e0069700e205c3a8c29066
Young <vacqx2983@gmail.com>
2025-12-13 21:47:29 -0500

    Proto front blue/front red revise

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonConstants.java
@@ -13,15 +13,27 @@ public class AutonConstants {
     public static double transitionWait = 0;
 
     public static Pose blueFrontScorePose = new Pose(51, 87, Math.toRadians(310));
+    public static Pose blueFrontProtoScorePose = new Pose(57, 85, Math.toRadians(315));
     public static Pose blueFrontStartPose = new Pose(30, 136, Math.toRadians(270));
+    public static Pose blueFrontProtoStartPose = new Pose(17,117, Math.toRadians(135));
     public static Pose blueFrontEndPose = new Pose(30, 48, Math.toRadians(180));
+
+    public static Pose blueFrontProtoEndPose = new Pose(60, 104, Math.toRadians(180));
     public static Pose blueFrontPickUpOneStage = new Pose(42, 74, Math.toRadians(180));
+
+    public static Pose blueFrontProtoPickUpOneStage = new Pose(56,80, Math.toRadians(180));
     public static Pose blueFrontPickUpOne = new Pose(10, 74, Math.toRadians(180));
+
+    public static Pose blueFrontProtoPickUpOne = new Pose(25,84, Math.toRadians(180));
     public static Pose blueFrontPickUpTwoStage = new Pose(42, 50, Math.toRadians(180));
+
+    public static Pose blueFrontProtoPickUpTwoStage = new Pose(56,58, Math.toRadians(180));
     public static Pose blueFrontPickUpTwo = new Pose(10, 50, Math.toRadians(180));
+
+    public static Pose blueFrontProtoPickUpTwo = new Pose(24,61,Math.toRadians(180));
     public static Pose redFrontStartPose = new Pose(114, 136, Math.toRadians(270));
 
-    public static Pose redFrontProtoStartPose = new Pose(87, 104, Math.toRadians(225));
+    public static Pose redFrontProtoStartPose = new Pose(115, 128, Math.toRadians(225));
     public static Pose redFrontScorePose = new Pose(88, 78, Math.toRadians(230));
 
     public static Pose redFrontProtoScorePose = new Pose(78,104, Math.toRadians(225));
@@ -30,17 +42,17 @@ public class AutonConstants {
     public static Pose redFrontProtoEndPose = new Pose(87,104, Math.toRadians(0));
     public static Pose redFrontSafePose = new Pose(120, 100, Math.toRadians(180));
 
-    public static Pose redFrontPickUpOneStage = new Pose(85, 98, Math.toRadians(0));
-    public static Pose redFrontProtoPickUpOneStage = new Pose(87, 80, Math.toRadians(0));
-    public static Pose redFrontPickUpOne = new Pose(115, 98, Math.toRadians(0));
+    public static Pose redFrontPickUpOneStage = new Pose(85, 83, Math.toRadians(180));
+    public static Pose redFrontProtoPickUpOneStage = new Pose(87, 80, Math.toRadians(180));
+    public static Pose redFrontPickUpOne = new Pose(118, 104, Math.toRadians(180));
 
-    public static Pose redFrontProtoPickUpOne = new Pose(126, 80, Math.toRadians(0));
+    public static Pose redFrontProtoPickUpOne = new Pose(118, 80, Math.toRadians(180));
     public static Pose redFrontPickUpTwoStage = new Pose(85, 74, Math.toRadians(0));
 
-    public static Pose redFrontProtoPickUpTwoStage = new Pose(78,57, Math.toRadians(0));
+    public static Pose redFrontProtoPickUpTwoStage = new Pose(87,58, Math.toRadians(0));
     public static Pose redFrontPickUpTwo = new Pose(115, 74, Math.toRadians(0));
 
-    public static Pose redFrontProtoPickUpTwo = new Pose(130,57,Math.toRadians(0));
+    public static Pose redFrontProtoPickUpTwo = new Pose(120,61,Math.toRadians(0));
     public static Pose redBackStartPose = new Pose(87.5, 9, Math.toRadians(270));
     public static Pose redBackScorePose = new Pose(85, 13, Math.toRadians(248));
     public static Pose redBackEndPose = new Pose(88, 36, Math.toRadians(180));
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueFrontReloadDoubleProto.java
@@ -0,0 +1,155 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoEndPose;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoPickUpOne;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoPickUpOneStage;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoPickUpTwo;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoPickUpTwoStage;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoStartPose;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.blueFrontProtoScorePose;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.shootingTime;
+import static org.firstinspires.ftc.teamcode.auton.AutonConstants.transitionWait;
+
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.ShootingPosition;
+import org.firstinspires.ftc.teamcode.subsystems.Gate;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.Launcher;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.delays.WaitUntil;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.extensions.pedro.PedroComponent;
+
+@Autonomous(name = "blue - Front Zone - Reload Double", group = "1")
+public class BlueFrontReloadDoubleProto extends VikingForceAutonBase {
+
+    private Path scorePreload;
+
+    private PathChain moveToPickUpOne;
+
+    private PathChain doPickUpOne;
+
+    private PathChain scorePickUpOne;
+
+    private PathChain moveToPickUpTwo;
+
+    private PathChain doPickUpTwo;
+
+    private PathChain scorePickUpTwo;
+    private PathChain leave;
+
+    public BlueFrontReloadDoubleProto() {
+        super(blueFrontProtoStartPose, ShootingPosition.TOP);
+    }
+
+    public BlueFrontReloadDoubleProto(Pose startPose, ShootingPosition shootingPosition) {
+        super(startPose, shootingPosition);
+    }
+
+    Command autonomousRoutine() {
+        return new SequentialGroup(
+                new ParallelGroup(
+                        Launcher.INSTANCE.start,
+                        new FollowPath(scorePreload, true, 1.00),
+                        IntakeSubsystem.INSTANCE.start
+                ),
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new FollowPath(moveToPickUpOne, false, 1.00),
+                new FollowPath(doPickUpOne, false, 1.00),
+                new FollowPath(scorePickUpOne, true, 1.00),
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+//                new TurnTo(Angle.fromDeg(0)),
+                new FollowPath(moveToPickUpTwo, true, 0.5
+
+
+                ),
+                new Delay(transitionWait),
+                new FollowPath(doPickUpTwo, true, 1.00),
+                new Delay(transitionWait),
+                new FollowPath(scorePickUpTwo, true, 1.00),
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new WaitUntil(Launcher.INSTANCE::nearGoal),
+                Gate.INSTANCE.open,
+                new Delay(shootingTime),
+                Gate.INSTANCE.close,
+                new ParallelGroup(
+                        Launcher.INSTANCE.stop,
+                        IntakeSubsystem.INSTANCE.stop,
+                        new FollowPath(leave, true, 1.00)
+                )
+
+        );
+    }
+
+    void buildPaths() {
+        scorePreload = new Path(new BezierLine(blueFrontProtoStartPose, blueFrontProtoScorePose));
+        scorePreload.setLinearHeadingInterpolation(blueFrontProtoStartPose.getHeading(), blueFrontProtoScorePose.getHeading());
+
+        moveToPickUpOne = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoScorePose, blueFrontProtoPickUpOneStage))
+                .setLinearHeadingInterpolation(blueFrontProtoScorePose.getHeading(), blueFrontProtoPickUpOneStage.getHeading()).build();
+
+        doPickUpOne = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoPickUpOneStage, blueFrontProtoPickUpOne))
+                .setLinearHeadingInterpolation(blueFrontProtoPickUpOneStage.getHeading(), blueFrontProtoPickUpOne.getHeading()).build();
+
+        scorePickUpOne = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoPickUpOne, blueFrontProtoScorePose))
+                .setLinearHeadingInterpolation(blueFrontProtoPickUpOne.getHeading(), blueFrontProtoScorePose.getHeading()).build();
+
+        moveToPickUpTwo = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoScorePose, blueFrontProtoPickUpTwoStage))
+                .setLinearHeadingInterpolation(blueFrontProtoScorePose.getHeading(), blueFrontProtoPickUpTwoStage.getHeading()).build();
+
+        doPickUpTwo = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoPickUpTwoStage, blueFrontProtoPickUpTwo))
+                .setLinearHeadingInterpolation(blueFrontProtoPickUpTwoStage.getHeading(), blueFrontProtoPickUpTwo.getHeading()).build();
+
+        scorePickUpTwo = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoPickUpTwo, blueFrontProtoScorePose))
+                .setLinearHeadingInterpolation(blueFrontProtoPickUpTwo.getHeading(), blueFrontProtoScorePose.getHeading()).build();
+
+        leave = PedroComponent.follower().pathBuilder()
+                .addPath(new BezierLine(blueFrontProtoScorePose, blueFrontProtoEndPose))
+                .setLinearHeadingInterpolation(blueFrontProtoScorePose.getHeading(), blueFrontProtoEndPose.getHeading()).build();
+        ;
+    }
+}
